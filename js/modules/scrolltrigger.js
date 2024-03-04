08b63cb9efae655dac5b0d3e49a0d3088ccbcb34
kwenox25 <113370944+kwenox25@users.noreply.github.com>
2024-03-04 01:26:08 -0500

    completed adding contents on why tuon section
    the ordering in mobile doesn't behave the way it should be but I'll fixed it later, just minor issue

--- js/modules/scrolltrigger.js
+++ js/modules/scrolltrigger.js
@@ -18,4 +18,19 @@ export function scrollTrigger() {
     link.addEventListener("click", scrollLink);
   });
   console.log("scroll to working");
+
+  const imageAnimation = gsap.from("#why-tuon-con img", {
+    opacity: 0,
+    x: 100,
+    stagger: 0.3,
+    duration: 3,
+    ease: "power4.out",
+  });
+
+  ScrollTrigger.create({
+    trigger: "#why-tuon-con",
+    animation: imageAnimation,
+    start: "top 80%",
+    once: true,
+  });
 }
